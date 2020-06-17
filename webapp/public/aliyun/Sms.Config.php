b1649ea5fd85728a107aca8e99a519aeae055e32
gzost <34446024+gzost@users.noreply.github.com>
2020-06-17 10:57:52 +0800

    Sms.config

--- webapp/public/aliyun/Sms.Config.php
+++ webapp/public/aliyun/Sms.Config.php
@@ -1,6 +1,8 @@
 <?php
 //用户av365的key
-define('ALI_AccKey', 'LTAIpJbtY9z01MkR');
-define('ALI_AccSecret', '73jzi5vFn1Tchu7Ahs1T502k9fmy9S');
+//define('ALI_AccKey', 'LTAIpJbtY9z01MkR');
+//define('ALI_AccSecret', '73jzi5vFn1Tchu7Ahs1T502k9fmy9S');
+
+
 
 ?>

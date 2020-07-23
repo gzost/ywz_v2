e3dcb7a8f4d0111b0777853e8beacd269fda0cd4
gzost <34446024+gzost@users.noreply.github.com>
2020-07-23 17:04:51 +0800

    Update Sms.Config.php

--- webapp/public/aliyun/Sms.Config.php
+++ webapp/public/aliyun/Sms.Config.php
@@ -4,5 +4,8 @@
 //define('ALI_AccSecret', '73jzi5vFn1Tchu7Ahs1T502k9fmy9S');
 
 
+define('ALI_AccKey', 'LTAI4GDiewWfAHW2KivVxNHG');
+define('ALI_AccSecret', 'cOWGwHNbGNYOGTRXpjJC4JZ73M08xF');
+
 
 ?>
